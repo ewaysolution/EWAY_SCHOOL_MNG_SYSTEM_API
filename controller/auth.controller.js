74828b3e0e8cfa9076bcd2fec108e659327d1b63
ewaysolution <help.eway.solutions@gmail.com>
2023-12-19 19:58:37 +0530

    Complete project

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -4,6 +4,7 @@ import { errorHandler } from "../util/error.js";
 import jwt from "jsonwebtoken";
 export const schoolLogin = async (req, res, next) => {
   const { email, password } = req.body;
+  
   try {
     const school = await School.findOne({ email });
 
