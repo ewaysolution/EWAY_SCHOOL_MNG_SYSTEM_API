567940d7e8158047792a6499d9276406a54f7ab8
ewaysolution <help.eway.solutions@gmail.com>
2023-12-20 17:44:13 +0530

    Complete project

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 
- 
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
